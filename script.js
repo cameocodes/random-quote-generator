40fdbb290a7b613f0d979d53663c004d9f6f8f0e
Cameo <hello@aeglir.com.au>
2018-05-18 22:47:08 +1000

    Add quotes.json, generator functionality

---
+++ script.js
@@ -0,0 +1,18 @@
+const btnClick = document.querySelector('button')
+const para = document.querySelector('p')
+const head = document.querySelector('h3')
+const request = fetch('/quotes.json')
+
+request
+    .then(response => response.json())
+    .then(parsedQuotes => showQuote(parsedQuotes))
+    .catch(err => console.log(err.message))
+
+
+const showQuote = response => {
+    btnClick.addEventListener("click", function(){
+    const quoteNum = Math.floor((Math.random() * 1640) + 1)
+    head.innerText = response[quoteNum].quoteText
+    para.innerText = response[quoteNum].quoteAuthor
+})
+}
